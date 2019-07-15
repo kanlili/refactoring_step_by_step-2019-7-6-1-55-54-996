7f61c971401455224ce3109cd3bc74a4c7186f5a
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-15 15:43:44 +0800

    Refactor:change to static final int that police check driver method

--- src/test/java/com/tws/refactoring/DriverTest.java
+++ src/test/java/com/tws/refactoring/DriverTest.java
@@ -1,46 +1,43 @@
 package com.tws.refactoring;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PoliceTest {
-    private  static Police police;
+public class DriverTest {
+    private  static Driver driver;
     @BeforeAll
     public  static void init(){
-        police=new Police();
+        driver= new Driver();
     }
+
     @Test
     public void should_return_true_when_driver_over_age() {
         //given
 
-        Driver driver=new Driver(19);
+        driver.setAge(19);;
         //when
-        boolean result=police.checkDriver(driver);
+        boolean result=driver.isLegalDriver();
         //then
         Assertions.assertEquals(true,result);
     }
     @Test
-    public void should_return_false_when_driver_low_age() {
+    public void should_return_true_when_driver_low_age() {
         //given
 
-        Driver driver=new Driver(17);
+         driver.setAge(18);
         //when
-        boolean result=police.checkDriver(driver);
+        boolean result=driver.isLegalDriver();
         //then
-        Assertions.assertEquals(false,result);
+        Assertions.assertEquals(true,result);
     }
     @Test
     public void should_return_true_when_driver_equal_age() {
         //given
 
-        Driver driver=new Driver(18);
+        driver.setAge(17);
         //when
-        boolean result=police.checkDriver(driver);
+        boolean result=driver.isLegalDriver();
         //then
-        Assertions.assertEquals(true,result);
-
+        Assertions.assertEquals(false,result);
     }
 }
+
